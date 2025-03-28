e6715d5280157f57db01dfb8ae600c03af974ebe
thackmaster <westhacker@icloud.com>
2025-03-28 23:26:14 +0000

    package update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://elkradio.com",
   integrations: [mdx(), sitemap(), tailwind()],
+  server: { host: '0.0.0.0', port: 4321 },
 });
